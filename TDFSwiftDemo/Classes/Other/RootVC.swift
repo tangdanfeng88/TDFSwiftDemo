e8803760f1a34e3df958383a14fc3272749f29d5
汤丹峰 <1064494857@qq.com>
2018-05-22 23:41:11 +0800

    初始化

---
+++ TDFSwiftDemo/Classes/Other/RootVC.swift
@@ -0,0 +1,18 @@
+//
+//  RootVC.swift
+//  TDFSwiftDemo
+//
+//  Created by 汤丹峰 on 2018/5/22.
+//  Copyright © 2018年 tangdanfeng. All rights reserved.
+//
+
+import UIKit
+class RootVC: UIViewController {
+    
+    override func viewDidLoad() {
+        self.title = "SwiftDemo"
+        self.view.backgroundColor = UIColor.white
+        
+        
+    }
+}
